c3fc57ac07904ae482a017606806d2aa664fbddc
Joel Weidenmark <joel.weidenmark@forefront.se>
2025-09-10 19:24:35 +0200

    removed some tests

--- src/L-liskov-substitution/rental-apartments.test.ts
+++ src/L-liskov-substitution/rental-apartments.test.ts
@@ -6,34 +6,32 @@ import {
 } from "./rental-apartments";
 
 describe("Rental Apartments", () => {
+  // Kommentera ut dessa när du löst uppgiften om du vill testa din lösning
   test("findApartments should return all luxury available apartments", () => {
-    const apartment1 = new RentalApartment();
-    const apartment2 = new LuxuryRentalApartment();
-    const apartment3 = new BigRentalApartment();
-    const apartments = [apartment1, apartment2, apartment3];
-
-    const availableApartments = findApartments(apartments, "Luxury");
-    expect(availableApartments).toHaveLength(1);
+    // const apartment1 = new RentalApartment();
+    // const apartment2 = new LuxuryRentalApartment();
+    // const apartment3 = new BigRentalApartment();
+    // const apartments = [apartment1, apartment2, apartment3];
+    // const availableApartments = findApartments(apartments, "Luxury");
+    // expect(availableApartments).toHaveLength(1);
   });
 
   test("findApartments should return all big available apartments", () => {
-    const apartment1 = new BigRentalApartment();
-    const apartment2 = new BigRentalApartment();
-    const apartment3 = new BigRentalApartment();
-    const apartments = [apartment1, apartment2, apartment3];
-
-    const availableApartments = findApartments(apartments, "Big");
-    expect(availableApartments).toHaveLength(3);
+    // const apartment1 = new BigRentalApartment();
+    // const apartment2 = new BigRentalApartment();
+    // const apartment3 = new BigRentalApartment();
+    // const apartments = [apartment1, apartment2, apartment3];
+    // const availableApartments = findApartments(apartments, "Big");
+    // expect(availableApartments).toHaveLength(3);
   });
 
   test("findApartments should return all apartments if the selected type does not exist", () => {
-    const apartment1 = new BigRentalApartment();
-    const apartment2 = new BigRentalApartment();
-    const apartment3 = new BigRentalApartment();
-    const apartment4 = new RentalApartment();
-    const apartments = [apartment1, apartment2, apartment3, apartment4];
-
-    const availableApartments = findApartments(apartments, "Luxury");
-    expect(availableApartments).toHaveLength(4);
+    // const apartment1 = new BigRentalApartment();
+    // const apartment2 = new BigRentalApartment();
+    // const apartment3 = new BigRentalApartment();
+    // const apartment4 = new RentalApartment();
+    // const apartments = [apartment1, apartment2, apartment3, apartment4];
+    // const availableApartments = findApartments(apartments, "Luxury");
+    // expect(availableApartments).toHaveLength(4);
   });
 });
